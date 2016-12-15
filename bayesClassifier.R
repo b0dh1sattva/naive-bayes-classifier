5d9ff4a8f5f3ae7dd7a51f1ff8f38a2455ee0ed7
Tim Dunbar <tim.dunbar@gmail.com>
2016-12-14 21:07:54 -0500

    changed algorithm in bayesClassifier to MNB

--- bayesClassifier.R
+++ bayesClassifier.R
@@ -1,32 +1,29 @@
-bayesClassifier<-function(menQ, womenQ, quote, menP, womenP){
+bayesClassifier<-function(menClass, womenClass, document, menPrior, womenPrior){
   #gets row count of men and women data frames
-  wCount<-nrow(womenQ)
-  mCount<-nrow(menQ)
-  #adds frequency column to men and women data frames
-  menQ<-as.data.frame(table(menQ))
-  womenQ<-as.data.frame(table(womenQ))
-  #finds intersection of quote data frame and the above data frames
-  intersectM<-menQ[is.element(menQ$menQ, intersect(quote$`unlist(x)`, menQ$menQ)),]
-  intersectW<-womenQ[is.element(womenQ$womenQ, intersect(quote$`unlist(x)`, womenQ$womenQ)),]
-  #get row count of men and women intersects
-  iCountM<-nrow(intersectM)
-  iCountW<-nrow(intersectW)
-  #find the likelihoods for each hypothesis only considering the words that match 
-  likelihoodM<-iCountM/(iCountM+iCountW)
-  likelihoodW<-iCountW/(iCountM+iCountW)
-  #probability that each word will occur in the individual dataframes
-  intersectM$Freq<-intersectM$Freq/mCount
-  intersectW$Freq<-intersectW$Freq/wCount
-  #likelihood that the word will occur in the words that match
-  intersectM$Freq<-intersectM$Freq*likelihoodM
-  intersectW$Freq<-intersectW$Freq*likelihoodW
-  #sums the frequency column which contains the individual posteriors
-  posteriorM<-sum(intersectM$Freq)
-  posteriorW<-sum(intersectW$Freq)
+  wCount<-nrow(womenClass)
+  mCount<-nrow(menClass)
+  #adds frequency column to menClass and womenClass data frames and calculates the frequency each word appeards in the classes
+  #This also compacts the the like words together in one row so we can get vocabulary size
+  menClass<-as.data.frame(table(menClass))
+  womenClass<-as.data.frame(table(womenClass))
+  #get the voacbulary size of each class
+  vocabM<-nrow(menClass)
+  vocabW<-nrow(womenClass)
+  #finds intersection of document data frame and the manClass and womanClass dataframes
+  intersectM<-menClass[is.element(menClass$menClass, intersect(document$`unlist(x)`, menClass$menClass)),]
+  intersectW<-womenClass[is.element(womenClass$womenClass, intersect(document$`unlist(x)`, womenClass$womenClass)),]
+  #conditional probabilities that each intersecting word, this would be the place to add smoothing if desired in place of the 0s
+  intersectM$Freq<-(intersectM$Freq+0)/(mCount+vocabM+0)
+  intersectW$Freq<-(intersectW$Freq+0)/(wCount+vocabW+0)
+  #finds product the frequency column and multiplies by the priors
+  posteriorM<-prod(intersectM$Freq)*menPrior
+  posteriorW<-prod(intersectW$Freq)*womenPrior
   #test for higher posterior
   if(posteriorW>posteriorM){
+    answer<-"Woman"
     return("Woman")
   }
+  answer<-"Man"
   return("Man")
 }
 
